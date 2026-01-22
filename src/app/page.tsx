821d9b3d45b3fb243c9032be58a6b0028665b757
callyberz <calvin.lee.to@gmail.com>
2026-01-22 09:14:46 -0500

    chore: remove content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
 'use client'
 
 import { type NextPage } from 'next'
-import React, { useEffect, useState } from 'react'
 import { TypographyH1 } from '@/components/common/TypographyH1'
-import { highlights, projects, summary } from '@/lib/data'
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card'
-import { GithubIcon, Link } from 'lucide-react'
+import { highlights, summary } from '@/lib/data'
 import { PostCard } from '@/components/PostCard'
 import { posts } from '#site/posts'
 import { compareDesc } from 'date-fns'
@@ -63,45 +55,6 @@ const Home: NextPage = () => {
         <div className="grid grid-cols-1 gap-4 py-4 md:grid-cols-2">
           {latestPosts.map((post, idx) => (
             <PostCard key={idx} {...post} />
-          ))}
-        </div>
-      </div>
-
-      <div className="mt-10 border-t border-gray-200 pt-10 dark:border-gray-700">
-        <TypographyH1>Projects</TypographyH1>
-        <div className="grid gap-4 py-4 md:grid-cols-2">
-          {projects.map(({ name, description, url, github }, index) => (
-            <motion.div
-              whileHover={{ scale: 1.05, rotate: 5 }}
-              whileTap={{ scale: 0.95 }}
-              key={index}
-              className="min-h-5"
-            >
-              <Card className="h-full">
-                <CardHeader>
-                  <CardTitle>
-                    {name}
-                    {url ? (
-                      <Link
-                        className="ml-2 inline-block h-5 w-5 cursor-pointer"
-                        onClick={() => {
-                          window.open(url, '_blank')
-                        }}
-                      />
-                    ) : null}
-                    {github ? (
-                      <GithubIcon
-                        className="ml-2 inline-block h-5 w-5 cursor-pointer"
-                        onClick={() => {
-                          window.open(github, '_blank')
-                        }}
-                      />
-                    ) : null}
-                  </CardTitle>
-                  <CardDescription>{description}</CardDescription>
-                </CardHeader>
-              </Card>
-            </motion.div>
           ))}
         </div>
       </div>
